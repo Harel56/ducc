ea2566dbb2008da8e4add3da70cdd2e15441fd3b
Harel56 <57253650+Harel56@users.noreply.github.com>
2020-05-25 12:01:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup, find_packages
-setup(name='ducc', vertion='0.1.0', author='Harel Etgar', description='project for course advanced system design', packages=find_packages(), install_requires=['sphinx', 'click', 'flask', 'Pillow', 'matplotlib', 'pika', 'pymongo'], tests_require=['pytest'])
+setup(name='ducc', vertion='1.0.0', author='Harel Etgar', description='Final project for course advanced system design', packages=find_packages(), install_requires=['sphinx', 'click', 'flask', 'Pillow', 'matplotlib', 'pika', 'pymongo', 'protobuf'], tests_require=['pytest'])
